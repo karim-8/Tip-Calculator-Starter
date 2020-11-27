a88e33314aa6fbabb7d3b53bea9418b6e8a27b6e
Karim <KariimAhmed8@gmail.com>
2020-11-27 18:03:14 +0200

    #Adding Application Assets

---
+++ lib/app_colors/app_colors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+const Color gradientStartColor = Color(0xff636FA4);
+const Color gradientEndColor = Color(0xffE8CBC0);
+const Color gradientStartColor2 = Color(0xffee9ca7);
+const Color gradientEndColor2 = Color(0xffffdde1);
